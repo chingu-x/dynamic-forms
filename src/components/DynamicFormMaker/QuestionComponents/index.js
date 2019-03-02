760543aa531113398fdced556bf4a348e14f20c8
The-Vampiire <25523682+the-vampiire@users.noreply.github.com>
2019-03-02 16:16:32 -0500

    DFC: copy over initial work from Chingu Frontend

---
+++ src/components/DynamicFormMaker/QuestionComponents/index.js
@@ -0,0 +1,26 @@
+import checkbox from "./checkbox";
+import checkbox_2_column from "./checkbox_2_column";
+import dropdown from "./dropdown";
+import radio from "./radio";
+import text from "./text";
+import textarea from "./textarea";
+import team_progress_sentiment_buttons from "./team_progress_sentiment_buttons.jsx";
+import voyage_application_tier_select from "./voyage_application_tier_select";
+import three_buttons from './three_buttons';
+import skill_setter from './SkillSetter';
+import multiple_text_input from './multiple_text_input';
+
+export default {
+  checkbox,
+  checkbox_2_column,
+  dropdown,
+  radio,
+  text,
+  textarea,
+// -- CUSTOM -- //
+  voyage_application_tier_select,
+  team_progress_sentiment_buttons,
+  three_buttons,
+  skill_setter,
+  multiple_text_input
+};
